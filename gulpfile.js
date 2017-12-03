8690b89dc9660819b2f7ce18051bfc73b621b2b7
Ivan Augusto <ivanaugustobd@gmail.com>
2017-12-03 17:54:26 -0200

    Added support to autoprefixer and clean-css

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,20 @@
 const gulp = require('gulp')
 
 const sass = require('gulp-sass')
-const stylesSrc = 'src/scss'
-const stylesDist = 'dist/css'
+const autoprefixer = require('gulp-autoprefixer')
+const cleanCss = require('gulp-clean-css')
 
-gulp.task('default', () => {
-  return gulp.src(`${stylesSrc}/animated-icons.scss`)
+const paths = {
+  src: 'src/scss',
+  dest: 'dist/css'
+}
+
+gulp.task('default', ['styles'])
+
+gulp.task('styles', () => {
+  return gulp.src(`${paths.src}/animated-icons.scss`)
     .pipe(sass())
-    .pipe(gulp.dest(stylesDist))
+    .pipe(autoprefixer())
+    .pipe(cleanCss({level: {2: {all: true}}}))
+    .pipe(gulp.dest(paths.dest))
 })
