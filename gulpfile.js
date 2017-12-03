edb65df84baca7e22043c734f87cbb942493046d
Ivan Augusto <ivanaugustobd@gmail.com>
2017-12-03 19:42:55 -0200

    Added html skeleton with live reload for development

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,26 @@
 const gulp = require('gulp')
 
+const browserSync = require('browser-sync').create()
+
 const sass = require('gulp-sass')
 const autoprefixer = require('gulp-autoprefixer')
 const cleanCss = require('gulp-clean-css')
 const sourcemaps = require('gulp-sourcemaps')
 
 const paths = {
+  public: 'public',
   src: 'src/scss',
-  dest: 'dist/css'
+  dest: 'public/dist/css'
 }
+
+gulp.task('serve', ['dev'], () => {
+  browserSync.init({
+    server: paths.public,
+    files: `${paths.dest}/*.css`
+  })
+
+  gulp.watch(`${paths.public}/index.html`).on('change', browserSync.reload)
+})
 
 gulp.task('styles:prod', ['styles:dev'], () => {
   return gulp.src(`${paths.dest}/animated-icons.css`)
@@ -31,6 +43,6 @@ gulp.task('dev', ['styles:dev'])
 
 gulp.task('prod', ['styles:prod'])
 
-gulp.task('watch', () => gulp.watch(`${paths.src}/**/*.scss`, ['styles:dev']))
+gulp.task('watch', ['serve'], () => gulp.watch(`${paths.src}/**/*.scss`, ['styles:dev']))
 
 gulp.task('default', ['prod'])
